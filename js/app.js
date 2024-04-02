bb6cfc12795147a1e4a9e0e6c428446bdc828a9e
Seun Dada <seun.dada777@gmail.com>
2024-04-02 13:59:40 -0400

    declare variables and assign cached elements to variables

---
+++ js/app.js
@@ -0,0 +1,27 @@
+//-----------constants---------//
+
+
+
+//------------variables--------//
+
+let snake = [{ x: 0, y: 0 }]
+let food = [{ x: 15, y: 15}]
+let score = 0
+let scoreHistory = []
+let dx = 0
+let dy = 0
+
+//--------cached elements---------//
+
+const gameBoard = document.querySelector("#gameBoard")
+const scoreEl = document.querySelector("#score")
+const scoreList = document.querySelector("#scoreList")
+const startGameBtn = document.querySelector("startGameBtn")
+
+//-----------event listners-------//
+
+//-----------functions-----------//
+
+function gameBoard() {
+  
+}
